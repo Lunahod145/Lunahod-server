56d836cb41f5edbdee39956f6ab180fd8b9cbe46
LEv145 <you@example.com>
2022-11-20 16:17:20 +0200

    v 0.1.1
    Fix urls
    Add `.gitignore`

--- src/lunohod_server/__init__.py
+++ src/lunohod_server/__init__.py
@@ -5,8 +5,9 @@ from .routers import LunohodRoutable
 
 
 app = FastAPI()
-routable_routers = [
+routable_routers = (
     LunohodRoutable(__import__("unittest.mock").mock.Mock()),  # TODO
-]
+)
+
 for routable_router in routable_routers:
     app.include_router(routable_router.router)
